9f4117105d2f17f066839c6a681ff419a3cb3285
FaerBo11 <pavel.pavlo2003@gmail.com>
2024-12-24 20:55:27 +0300

    Update game2.py
    ЙОУ

--- game2.py
+++ game2.py
@@ -9,7 +9,9 @@ class Board:
         self.left = 10
         self.top = 10
         self.cell_size = 30
-        self.player = 0, 0
+        self.x = 0
+        self.y = 0
+        self.player = self.board[self.x][self.y] = 1
 
     def set_view(self, left, top, cell_size):
         self.left = left
@@ -25,28 +27,43 @@ class Board:
                         self.cell_size),
                                      width=1)
                 elif self.board[i][j] == 1:
-                    pygame.draw.circle(screen, "red", ((self.left + (j * self.cell_size)) + self.cell_size / 2, (self.top + (i * self.cell_size)) + self.cell_size / 2),
-                                     self.cell_size / 2 - 2,
-                                     width=2)
+                    pygame.draw.circle(screen, "red", ((self.left + (j * self.cell_size)) + self.cell_size / 2,
+                                                       (self.top + (i * self.cell_size)) + self.cell_size / 2),
+                                       self.cell_size / 2 - 2,
+                                       width=2)
                     pygame.draw.rect(screen, "white", (
                         self.left + (j * self.cell_size), self.top + (i * self.cell_size), self.cell_size,
                         self.cell_size),
                                      width=1)
 
-
     def down(self):
-        if self.board[coord[0]][coord[1] + 1] != 2:
-            self.board[coord[0]][coord[1] + 1] = self.player[coord[0]][1] + 1
+        self.board[self.x][self.y] = 0
+        self.x += 1
+        self.board[self.x][self.y] = self.player
+        for i in range(len(self.board)):
+            print(self.board[i])
 
+    def up(self):
+        self.board[self.x][self.y] = 0
+        self.x -= 1
+        self.board[self.x][self.y] = self.player
+        for i in range(len(self.board)):
+            print(self.board[i])
 
+    def left(self):
+        self.board[self.x][self.y] = 0
+        self.y -= 1
+        self.board[self.x][self.y] = self.player
+        for i in range(len(self.board)):
+            print(self.board[i])
 
-    def get_cells(self):
-            return (self.x - self.left) // self.cell_size, (self.y - self.top) // self.cell_size
+    def right(self):
+        self.board[self.x][self.y] = 0
+        self.y += 1
+        self.board[self.x][self.y] = self.player
+        for i in range(len(self.board)):
+            print(self.board[i])
 
-    def click(self):
-        coord = self.get_cells()
-        self.move(coord)
-        self.render(screen)
 
 
 if __name__ == '__main__':
@@ -60,52 +77,20 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = event.pos
-                board.click()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    board.right()
-                    print(1)
-                if event.key == pygame.K_LEFT:
-                    board.left()
-                    print(2)
-                if event.key == pygame.K_UP:
-                    board.up()
-                    print(3)
                 if event.key == pygame.K_DOWN:
                     board.down()
+                    print(4)
+                if event.key == pygame.K_UP:
+                    board.up()
+                    print(4)
+                if event.key == pygame.K_LEFT:
+                    board.left()
+                    print(4)
+                if event.key == pygame.K_RIGHT:
+                    board.right()
                     print(4)
         screen.fill((0, 0, 0))
         board.render(screen)
         pygame.display.flip()
 
-
-'''if __name__ == '__main__':
-    pygame.display.set_caption('Чёрное в белое и наоборот')
-    size = width, height = 400, 400
-    screen = pygame.display.set_mode(size)
-    screen.fill('black')
-    board = Board(5, 7)
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = event.pos
-                print(x, y)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    x, y = 25, 25
-                    board.get_cells(x, y)
-                    print(1)
-                if event.key == pygame.K_LEFT:
-                    print(2)
-                if event.key == pygame.K_UP:
-                    print(3)
-                if event.key == pygame.K_DOWN:
-                    print(4)
-        screen.fill((0, 0, 0))
-        board.render(screen)
-        pygame.display.flip()'''
